bc081a9a74856b22d6dfbb880f62a8c5478597d2
MinChang <mwsfot@outlook.com>
2024-11-27 09:18:02 +0800

    first init

---
+++ barberShop-admin/src/main/java/com/mwsfot/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.mwsfot.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.mwsfot.common.core.controller.BaseController;
+import com.mwsfot.common.core.domain.AjaxResult;
+import com.mwsfot.common.core.domain.model.RegisterBody;
+import com.mwsfot.common.utils.StringUtils;
+import com.mwsfot.framework.web.service.SysRegisterService;
+import com.mwsfot.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
